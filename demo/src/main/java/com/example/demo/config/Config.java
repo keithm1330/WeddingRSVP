6086ca6b640ef747185b3e7a6c14316c46be4f8d
keithm1330 <keith13@live.ie>
2023-02-25 16:07:06 +0000

    Added email

---
+++ demo/src/main/java/com/example/demo/config/Config.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+@Configuration
+public class Config {
+    @Bean
+    public JavaMailSender emailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("keithsamanthaswedding@gmail.com");
+        mailSender.setPassword("rtiwkqmpdxzczbxq");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
--- demo/src/main/java/com/example/demo/controller/GreetingController.java
+++ demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.RSVPResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class GreetingController {
@@ -17,6 +21,7 @@ public class GreetingController {
     @PostMapping("rsvp")
     public String rsvpTest(@ModelAttribute RSVPResponse greeting, Model model) {
         model.addAttribute("RSVPResponse", greeting);
+        sendSimpleMessage("keithsamanthaswedding@gmail.com", "WEDDING RSVP", greeting);
         return "thankyou";
     }
 
@@ -26,11 +31,14 @@ public class GreetingController {
         return "rsvp";
     }
 
-//    @Autowired
-//    private JavaMailSender mailSender;
-//
-//    public void sendEmail() {
-//        // use mailSender here...
-//    }
+    @Autowired
+    private JavaMailSender emailSender;
 
+    public void sendSimpleMessage(String to, String subject, RSVPResponse text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText("People: "+text.getNames()+" attending: "+text.getAttending()+ "Message : "+text.getMessage());
+        emailSender.send(message);
+    }
 }
